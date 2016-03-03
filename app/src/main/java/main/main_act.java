3a747f5df0ef60ebe9fc3821716117e97bb09135
bakhoj <anders2108@gmail.com>
2016-03-03 17:05:06 +0100

    added host tab

--- app/src/main/java/main/main_act.java
+++ app/src/main/java/main/main_act.java
@@ -25,14 +25,7 @@ public class main_act extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -52,13 +45,6 @@ public class main_act extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.main_act, menu);
-        return true;
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
