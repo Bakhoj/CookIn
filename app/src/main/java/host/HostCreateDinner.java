c52bdc6ff664074be5ad766b082bb028a9bdacb2
LarsPede <larspeter94@gmail.com>
2016-05-03 16:45:37 +0200

    played around in that motherf***ing gradle!

--- app/src/main/java/host/HostCreateDinner.java
+++ app/src/main/java/host/HostCreateDinner.java
@@ -1,19 +1,21 @@
 package host;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.example.anders.cookin.R;
+import com.pixelcan.inkpageindicator.InkPageIndicator;
 
-public class HostCreateDinner extends AppCompatActivity {
+public class HostCreateDinner extends Fragment {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.host_create_dinner_act);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        InkPageIndicator inkPageIndicator = (InkPageIndicator) findViewById(R.id.indicator);
+        inkPageIndicator.setViewPager(viewPager);
     }
 
 }
