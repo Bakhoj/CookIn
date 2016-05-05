c2bab2932cf1e53ce69dd13e03c2d3f012cc39d5
LarsPede <larspeter94@gmail.com>
2016-05-05 15:18:41 +0200

    yes

--- app/src/main/java/host/create/HostCreateDinnerViewpagerFrag.java
+++ app/src/main/java/host/create/HostCreateDinnerViewpagerFrag.java
@@ -30,7 +30,7 @@ public class HostCreateDinnerViewpagerFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.viewpager_frag, container, false);
+        View root = inflater.inflate(R.layout.host_create_dinner_viewpager_frag, container, false);
 
         mSectionsPagerAdapter = new CreateSectionsPagerAdapter(getChildFragmentManager());
 
@@ -42,16 +42,7 @@ public class HostCreateDinnerViewpagerFrag extends Fragment {
 
         return root;
     }
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
