574e281591724fb06dba44baa8a0d3e582f653b6
bakhoj <anders2108@gmail.com>
2016-05-10 10:54:35 +0200

    HostCreateDinner

--- app/src/main/java/host/create/HostCreateDinner.java
+++ app/src/main/java/host/create/HostCreateDinner.java
@@ -16,7 +16,7 @@ public class HostCreateDinner extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_act);
+        setContentView(R.layout.host_create_dinner_act);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
