50741306bbe3e8131a3a9864238d3b088a2da7e4
LarsPede <larspeter94@gmail.com>
2016-05-12 13:31:03 +0200

    numberpicker

--- app/src/main/java/host/create/CreateFirstFrag.java
+++ app/src/main/java/host/create/CreateFirstFrag.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 import android.widget.ScrollView;
 
 import com.example.anders.cookin.R;
@@ -20,6 +21,13 @@ public class CreateFirstFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.create_first_frag, container, false);
 
+        rootView.findViewById(R.id.titleCreate).requestFocus();
+
+        NumberPicker np = (NumberPicker) rootView.findViewById(R.id.numberPickerGuests);
+        np.setMaxValue(100);
+        np.setMinValue(1);
+        np.setValue(1);
+        np.setWrapSelectorWheel(true);
 
 
         return rootView;
