e5770e4e61021d567634a2d48439a4fefeaecb07
anders <Anders2108@hotmail.com>
2016-05-16 16:29:48 +0200

    playing with list

--- app/src/main/java/data/Choice.java
+++ app/src/main/java/data/Choice.java
@@ -1,14 +1,11 @@
 package data;
 
-import java.util.Date;
-
 /**
  * Created by anders on 20-Mar-16.
  */
 public class Choice {
 
     public int recyclerExpandedPosition;
-    public Date startDate, endDate, deadlineDate;       //The last chosen dates for create host
     public int hostPosition;
 
     public Choice() {
