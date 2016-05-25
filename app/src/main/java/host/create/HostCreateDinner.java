4a9de24ce2c35a4440ccad74149dbe99c6a5339c
LarsPede <larspeter94@gmail.com>
2016-05-25 07:40:48 +0200

    createbutton done

--- app/src/main/java/host/create/HostCreateDinner.java
+++ app/src/main/java/host/create/HostCreateDinner.java
@@ -8,9 +8,15 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.cookin.app.R;
+
+import data.Banquet;
+import data.Data;
+import data.FireBHandler;
 
 public class HostCreateDinner extends AppCompatActivity {
 
@@ -31,10 +37,35 @@ public class HostCreateDinner extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.create_dinner_actionbar, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case android.R.id.home:
                 finish();
+                return true;
+            case R.id.create_action:
+
+                if(Data.getInstance().choice.getTitle().equals("")){
+                    System.out.println("shit son");
+                } else {
+
+                    Banquet banquet = new Banquet();
+                    banquet.setTitle(Data.getInstance().choice.getTitle());
+                    banquet.setDescription(Data.getInstance().choice.getDescription());
+                    banquet.setMaxGuest(Data.getInstance().choice.getGuest());
+                    banquet.setPricetag(Data.getInstance().choice.getPrice());
+                    banquet.setStartDate(Data.getInstance().choice.getStartDate());
+                    banquet.setDeadlineDate(Data.getInstance().choice.getDeadlineDate());
+                    FireBHandler.getInstance().uploadDinner(banquet);
+                    finish();
+                }
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -43,6 +74,5 @@ public class HostCreateDinner extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        System.out.println("motherfucking yes");
     }
 }
